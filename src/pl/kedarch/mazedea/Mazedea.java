8c326e47629690cc59b2c7ee45963e658e9a38b4
Radosław Woźniak <radoslaw.wozniak2000@gmail.com>
2022-11-01 16:40:12 +0100

    Change main class
    Now it is used only to run app.

--- src/pl/kedarch/mazedea/Mazedea.java
+++ src/pl/kedarch/mazedea/Mazedea.java
@@ -1,29 +1,16 @@
 package pl.kedarch.mazedea;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.InputStream;
-import java.io.File;
-import java.net.URL;
+
+/**
+ * Class used to start app
+ */
 public class Mazedea {
-    public static void main(String[] args) throws Exception {
-        String resDir = new Mazedea().getClass().getResource("resources/").toString();
-        System.out.println(resDir+"maps");
-        String appPath = new File(Mazedea.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getParentFile().getPath();
-        System.out.println(appPath);
-        URL file = new URL(resDir);
-        System.out.println(file);
-        BufferedReader d = new BufferedReader(new InputStreamReader(file.openStream()));
-        if (resDir.startsWith("jar:")) {
-            System.out.println("In jar");
-        } else {
-            System.out.println("Not in jar");
-        }
-        File files[] = new File(appPath).listFiles();
-        System.out.println(files.length);
-        System.out.println(files);
-        for (int i = 0; i < files.length; i++) {
-            System.out.println(files[i]);
-        }
-        Maze maze = new Maze(1, 1);
+    /**
+     * Function starting the app
+     * @param args
+     * command line arguments
+     */
+    public static void main(final String[] args) {
+        MazeController control = new MazeController();
+        control.start();
     }
 }
